dff304277dd00dad62aff28eb4b65b5c747202d8
12bhargava <156225130+12bhargava@users.noreply.github.com>
2024-03-26 21:34:58 -0500

    app

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pickRouter= require('./routes/pick');
+var racquetRouter= require('./routes/racquet');
+var gridRouter= require('./routes/grid');
 
 var app = express();
 
@@ -21,6 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/pick', pickRouter);
+app.use('/racquet', racquetRouter);
+app.use('/grid', gridRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
