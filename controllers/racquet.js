3b492c51a5095baabe19969699060534f61415cc
12bhargava <156225130+12bhargava@users.noreply.github.com>
2024-04-10 18:46:41 -0500

    Commit all

--- controllers/racquet.js
+++ controllers/racquet.js
@@ -65,9 +65,23 @@ res.send(`{"error": ${err}}`);
 };
 
 // Handle racquet delete from on DELETE.
-exports.racquet_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: racquet delete DELETE ' + req.params.id);
+// exports.racquet_delete = function(req, res) {
+// res.send('NOT IMPLEMENTED: racquet delete DELETE ' + req.params.id);
+// };
+//Handle Costume delete on DELETE.
+exports.racquet_delete = async function(req, res) {
+console.log("delete " + req.params.id)
+try {
+result = await racquet.findByIdAndDelete( req.params.id)
+console.log("Removed " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": Error deleting ${err}}`);
+}
 };
+
+
 // Handle racquet update form on PUT.
 // exports.racquet_update_put = function(req, res) {
 // res.send('NOT IMPLEMENTED: racquet update PUT' + req.params.id);
@@ -92,3 +106,61 @@ res.send(`{"error": ${err}: Update for id ${req.params.id}
 failed`);
 }
 };
+//Handle a show one view with id specified by query
+exports.racquet_view_one_Page = async function(req, res) {
+console.log("single view for id " + req.query.id)
+try{
+result = await racquet.findById( req.query.id)
+res.render('racquetdetail',
+{ title: 'racquet Detail', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.racquet_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('racquetcreate', { title: 'racquet Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle building the view for updating a costume.
+// query provides the id
+exports.racquet_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await racquet.findById(req.query.id)
+    res.render('racquetupdate', { title: 'racquet Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+    // Handle a delete one view with id from query
+exports.racquet_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await racquet.findById(req.query.id)
+    res.render('racquetdelete', { title: 'racquet Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+    
+    
--- routes/racquet.js
+++ routes/racquet.js
@@ -11,4 +11,9 @@ const racquet_controlers= require('../controllers/racquet');
 var router = express.Router();
 /* GET Racquets */
 router.get('/', racquet_controlers.racquet_view_all_Page );
+router.get('/detail', racquet_controlers.racquet_view_one_Page);
+router.get('/create', racquet_controlers.racquet_create_Page);
+router.get('/update', racquet_controlers.racquet_update_Page);
+router.get('/delete', racquet_controlers.racquet_delete_Page);
+
 module.exports = router;
